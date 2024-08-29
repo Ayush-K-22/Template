70fee632f0e9ab47e88a2b8412e21caca2e35647
Ayush Khandal <132454274+Ayush-K-22@users.noreply.github.com>
2024-08-29 12:06:07 +0530

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,15 +1,17 @@
 /*
     * Author : AyushK22
 */
- 
+
 #include<bits/stdc++.h>
 using namespace std;
+
 #define int long long
 #define vi vector <int>
 #define vb vector <bool>
 #define vp vector <pair <int, int>>
+#define all(x) x.begin(),x.end()
 
-void Dijkstra (int src, vi &dis, vector <vp> &adj, vb &vis) {
+void Dijkstra (int src, vi &dis, vector <vp> &adj, vb &vis, vi &par) {
     priority_queue <pair <int,int>, vp, greater <pair <int,int>>> pq;
     pq.push({0,src});
     while (!pq.empty()) {
@@ -22,7 +24,24 @@ void Dijkstra (int src, vi &dis, vector <vp> &adj, vb &vis) {
             if (dis[p.second] + it.second < dis[it.first]) {
                 pq.push({dis[p.second] + it.second, it.first});
                 dis[it.first] = dis[p.second] + it.second;
+                par[it.first] = p.second;
             }
         }
     }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    int n, m; cin >> n >> m;
+    vector <vp> adj (n+1);
+    for (int i = 0; i<m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+    }
+
+    vi dis (n+1, 1e16), par (n+1);
+    vb vis (n+1);
+    Dijkstra (1,dis,adj,vis,par);
 }
