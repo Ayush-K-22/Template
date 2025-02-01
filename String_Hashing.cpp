e4c1da044480dbde541492e2615a33c10b5eccf4
Ayush Khandal <132454274+Ayush-K-22@users.noreply.github.com>
2025-02-01 13:17:09 +0530

    RollingHash.cpp

--- String_Hashing.cpp
+++ String_Hashing.cpp
@@ -5,80 +5,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define vi vector <int>
-const int MOD1 = 1e9 + 7;
-const int MOD2 = 1e9 + 9;
-
-// To Find Power in O(1)
-int power [200002][2];
-
-void fill () {
-    int cur1 = 1, cur2 = 1;
-    for (int i = 0; i<2e5+2; i++) {
-        power[i][0] = cur1;
-        cur1 = (cur1*31)%MOD1;
-        power[i][1] = cur2;
-        cur2 = (cur2*31)%MOD2;
-    }
-}
-
-class String_Hash {        
-    public :
-        // f(x) = a1*x^(n-1) + a2*x^(n-2) + ... + a_n;
-
-        static int binpow(int a, int b, int m) {
-            a %= m;
-            int res = 1;
-            while (b > 0) {
-                if (b & 1)
-                    res = (res * a) % m;
-                a = (a * a) % m;
-                b >>= 1;
+class RollingHash {
+    private:
+        int n, MOD = 1e9 + 7;
+        vector <int> hash1, hash2, power1, power2;
+    public:
+        RollingHash (vector <int> &v) {
+            n = v.size();
+            hash1.resize(n + 1, 0); hash2.resize(n + 1, 0);
+            power1.resize(n, 1); power2.resize(n, 1);
+            for (int i = 1; i<n; i++) {
+                power1[i] = (1LL * power1[i-1] * 31) % MOD;
+                power2[i] = (1LL * power2[i-1] * 37) % MOD;
             }
-            return res;
+            for (int i = 1; i <= n; i++) {
+                hash1[i] = (1LL * hash1[i-1] + 1LL * v[i-1] * power1[n-i]) % MOD;
+                hash2[i] = (1LL * hash2[i-1] + 1LL * v[i-1] * power2[n-i]) % MOD;
+            }
         }
 
-        static int mod_div (int a, int b, int mod) {
-            return (a * binpow (b, mod - 2, mod)) % mod;
+        int modDiv (int x, int y) {
+            auto binpow = [&](int a, int b) {
+                int res = 1;
+                while (b > 0) {
+                    if (b & 1)
+                        res = (1LL * res * a) % MOD;
+                    a = (1LL * a * a) % MOD;
+                    b >>= 1;
+                }
+                return res;
+            };
+            return (1LL * x * binpow(y, MOD-2)) % MOD;
         }
 
-        static int getHash (string &s, int mod, int pol) {
-            int ans = 0, n = s.length();
-            for (int i = 0; i<s.length(); i++)
-                ans = (ans + ((s[i]-'a'+1) * power[n-1-i][pol])) % mod;
-            return ans;
+        pair <int,int> getHashVal (int l, int r) { // 1 - Based Indexing
+            int hash_val1 = (hash1[r] - hash1[l-1] + MOD) % MOD;
+            hash_val1 = modDiv(hash_val1, power1[n-r]);
+            int hash_val2 = (hash2[r] - hash2[l-1] + MOD) % MOD;
+            hash_val2 = modDiv(hash_val2, power2[n-r]);
+            return {hash_val1, hash_val2};
         }
 
-        static void fill_hash (vi &v, string &s, int mod, int pol) {
-            int n = s.length();
-            for (int i = 0; i<s.length(); i++)
-                v[i+1] = (v[i] + (s[i]-'a'+1) * power[n-1-i][pol]) % mod;
-        }
-
-        // Find wheather there is a substring with given target of length k
-        static bool isPossible (int tar, vi &hash, int k) {
-            int r = k;
-            while (r < hash.size()-1) {
-                int l = r - k + 1;
-                int num = (hash[r] - hash[l-1] + MOD1) % MOD1;
-                num = (num*power[l-1][0]) % MOD1;
-                if (num == tar) return true;
-                r++;
+        bool isPossible (pair <int,int> hash_val, int len) {
+            pair <int,int> temp;
+            for (int i = 1; i+len-1 <= n; i++) {
+                temp = getHashVal(i, i+len-1);
+                if (temp == hash_val) return true;
             }
             return false;
-        }
-
-        // 1 - Based Indexing
-        static int get_Val (int l, int r, int mod, vi &hash, string &s, int pol) {
-            int n = s.length();
-            int ans = (hash[r] - hash[l-1] + mod) % mod;
-            return mod_div (ans, power[n-r][pol],mod);
         }
 };
 
 int32_t main(){
     ios::sync_with_stdio(false), cin.tie(NULL);
-    fill();
     return 0;
 }
