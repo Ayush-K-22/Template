0124ae1c1fe6b1919ffc5a244b1c4afc2a448cde
Ayush Khandal <132454274+Ayush-K-22@users.noreply.github.com>
2025-07-27 01:01:38 +0530

    Update String_Hashing.cpp

--- String_Hashing.cpp
+++ String_Hashing.cpp
@@ -7,49 +7,31 @@ using namespace std;
 
 class RollingHash {
     private:
-        int n, MOD = 1e9 + 7;
+        int n, MOD = 1e9 + 7, base1 = 31, base2 = 37;
         vector <int> hash1, hash2, power1, power2;
     public:
-        RollingHash (vector <int> &v) {
-            n = v.size();
+        RollingHash (const string &s) {
+            n = s.size();
             hash1.resize(n + 1, 0); hash2.resize(n + 1, 0);
-            power1.resize(n, 1); power2.resize(n, 1);
-            for (int i = 1; i<n; i++) {
-                power1[i] = (1LL * power1[i-1] * 31) % MOD;
-                power2[i] = (1LL * power2[i-1] * 37) % MOD;
-            }
+            power1.resize(n + 1, 1); power2.resize(n + 1, 1);
             for (int i = 1; i <= n; i++) {
-                hash1[i] = (1LL * hash1[i-1] + 1LL * v[i-1] * power1[n-i]) % MOD;
-                hash2[i] = (1LL * hash2[i-1] + 1LL * v[i-1] * power2[n-i]) % MOD;
+                power1[i] = (1LL * power1[i - 1] * base1) % MOD;
+                power2[i] = (1LL * power2[i - 1] * base2) % MOD;
+                hash1[i] = (1LL * hash1[i - 1] * base1 + s[i - 1]) % MOD;
+                hash2[i] = (1LL * hash2[i - 1] * base2 + s[i - 1]) % MOD;
             }
         }
 
-        int modDiv (int x, int y) {
-            auto binpow = [&](int a, int b) {
-                int res = 1;
-                while (b > 0) {
-                    if (b & 1)
-                        res = (1LL * res * a) % MOD;
-                    a = (1LL * a * a) % MOD;
-                    b >>= 1;
-                }
-                return res;
-            };
-            return (1LL * x * binpow(y, MOD-2)) % MOD;
-        }
-
-        pair <int,int> getHashVal (int l, int r) { // 1 - Based Indexing
-            int hash_val1 = (hash1[r] - hash1[l-1] + MOD) % MOD;
-            hash_val1 = modDiv(hash_val1, power1[n-r]);
-            int hash_val2 = (hash2[r] - hash2[l-1] + MOD) % MOD;
-            hash_val2 = modDiv(hash_val2, power2[n-r]);
+        pair <int,int> getHashVal (int l, int r) { // 1-based Indexing
+            int hash_val1 = (hash1[r] - 1LL * hash1[l - 1] * power1[r - l + 1] % MOD + MOD) % MOD;
+            int hash_val2 = (hash2[r] - 1LL * hash2[l - 1] * power2[r - l + 1] % MOD + MOD) % MOD;
             return {hash_val1, hash_val2};
         }
 
         bool isPossible (pair <int,int> hash_val, int len) {
             pair <int,int> temp;
-            for (int i = 1; i+len-1 <= n; i++) {
-                temp = getHashVal(i, i+len-1);
+            for (int i = 1; i + len - 1 <= n; i++) {
+                temp = getHashVal(i, i + len - 1);
                 if (temp == hash_val) return true;
             }
             return false;
